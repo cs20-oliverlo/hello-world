fbf1b38a1a5964292dac611925bbcf549831a178
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-16 11:01:45 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-// JavaScript Code
+// Hello World Plus by Oliver Lo
 
 // IPO Algorithm (Input-Process-Output)
 
 // Input
 let username = prompt("What is your name?");
-let timeOfDay = prompt("What time is it?")
+let pizzaSize = prompt(`How large would you like your pizza to be, ${username}?`);
+let topping1 = prompt(`What would you like your first topping to be on your ${pizzaSize} pizza?`);
+let topping2 = prompt(`And what would you like your second topping to be?`);
+
 
 // Process
-let msg = `Hello ${username}. Have a great ${timeOfDay}`
+let msg = `Your ${pizzaSize} pizza with ${topping1} and ${topping2} will be ready in 20 minutes.`;
 
 // Output
 alert(msg)
+
+// Check you understanding
+// Modify/Change the code above to make it a Pizza Order
+// Ask the user for the sie of pizza and two different toppings
+// Output: Your ___ pizza with ___ and ___ will be ready in 20 minutes.
