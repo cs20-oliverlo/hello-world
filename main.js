940f0d3393b2362b550779c683e4af9729d18965
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-16 11:03:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Hello World Plus by Oliver Lo
+// Pizza Order by Oliver Lo
 
 // IPO Algorithm (Input-Process-Output)
 
@@ -14,8 +14,3 @@ let msg = `Your ${pizzaSize} pizza with ${topping1} and ${topping2} will be read
 
 // Output
 alert(msg)
-
-// Check you understanding
-// Modify/Change the code above to make it a Pizza Order
-// Ask the user for the sie of pizza and two different toppings
-// Output: Your ___ pizza with ___ and ___ will be ready in 20 minutes.
