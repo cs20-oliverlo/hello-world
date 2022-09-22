95f1d6a2ee0918556999d7092e180ae7fea32115
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-22 10:53:48 -0600

    awd

--- main.js
+++ main.js
@@ -2,15 +2,31 @@
 
 // IPO Algorithm (Input-Process-Output)
 
-// Input
-let username = prompt("What is your name?");
-let pizzaSize = prompt(`How large would you like your pizza to be, ${username}?`);
-let topping1 = prompt(`What would you like your first topping to be on your ${pizzaSize} pizza?`);
-let topping2 = prompt(`And what would you like your second topping to be?`);
+//     // Input
+// let username = prompt("What is your name?");
+// let pizzaSize = prompt(`How large would you like your pizza to be, ${username}?`);
+// let topping1 = prompt(`What would you like your first topping to be on your ${pizzaSize} pizza?`);
+// let topping2 = prompt(`And what would you like your second topping to be?`);
+
+
+// // Process
+// let msg = `Your ${pizzaSize} pizza with ${topping1} and ${topping2} will be ready in 20 minutes.`;
+
+// // Output
+// alert(msg)
+
+
+document.getElementById("btn").addEventListener("click", sayGreeting);
+
+function sayGreeting() {
+    // Input
+let username = document.getElementById("name-in").value;
+let timeOfDay = document.getElementById("time-in").value;
 
 
 // Process
-let msg = `Your ${pizzaSize} pizza with ${topping1} and ${topping2} will be ready in 20 minutes.`;
+let msg = `Hello ${username}. Have a great ${timeOfDay}.`;
 
 // Output
-alert(msg)
+document.getElementById("output").innerHTML = msg;
+}
